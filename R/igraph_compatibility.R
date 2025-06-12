283750920b009faf4af7257db07d3035975d32c7
giampaolo montaletti <giampaolo@montaletti.com>
2025-06-12 16:06:37 +0200

    Fix igraph compatibility layer function availability
    - Replace global assignment (<<-) with package environment (.moneca_env)
    - Define proper wrapper functions to access compatibility functions
    - Ensures moneca_graph_from_adjacency and other compatibility functions are accessible
    - Update version to 0.1.2
    
    This fixes the 'could not find function moneca_graph_from_adjacency' error.

--- R/igraph_compatibility.R
+++ R/igraph_compatibility.R
@@ -5,6 +5,9 @@
 #' 
 #' @keywords internal
 
+# Store compatibility functions in package environment
+.moneca_env <- new.env(parent = emptyenv())
+
 # Check igraph version and set up appropriate function mappings
 .setup_igraph_compat <- function() {
   igraph_version <- utils::packageVersion("igraph")
@@ -12,27 +15,27 @@
   # Version 1.3.0 introduced the new API with underscore notation
   if (igraph_version >= "1.3.0") {
     # Use new API functions
-    moneca_graph_from_adjacency <<- function(...) igraph::graph_from_adjacency_matrix(...)
-    moneca_get_edgelist <<- function(...) igraph::as_edgelist(...)
-    moneca_graph_density <<- function(...) igraph::edge_density(...)
-    moneca_shortest_paths <<- function(...) igraph::distances(...)
-    moneca_average_path_length <<- function(...) igraph::mean_distance(...)
-    moneca_components <<- function(...) igraph::components(...)
+    .moneca_env$moneca_graph_from_adjacency <- function(...) igraph::graph_from_adjacency_matrix(...)
+    .moneca_env$moneca_get_edgelist <- function(...) igraph::as_edgelist(...)
+    .moneca_env$moneca_graph_density <- function(...) igraph::edge_density(...)
+    .moneca_env$moneca_shortest_paths <- function(...) igraph::distances(...)
+    .moneca_env$moneca_average_path_length <- function(...) igraph::mean_distance(...)
+    .moneca_env$moneca_components <- function(...) igraph::components(...)
   } else {
     # Use old API functions
-    moneca_graph_from_adjacency <<- function(...) igraph::graph.adjacency(...)
-    moneca_get_edgelist <<- function(...) igraph::get.edgelist(...)
-    moneca_graph_density <<- function(...) igraph::graph.density(...)
-    moneca_shortest_paths <<- function(...) igraph::shortest.paths(...)
-    moneca_average_path_length <<- function(...) igraph::average.path.length(...)
-    moneca_components <<- function(...) igraph::clusters(...)
+    .moneca_env$moneca_graph_from_adjacency <- function(...) igraph::graph.adjacency(...)
+    .moneca_env$moneca_get_edgelist <- function(...) igraph::get.edgelist(...)
+    .moneca_env$moneca_graph_density <- function(...) igraph::graph.density(...)
+    .moneca_env$moneca_shortest_paths <- function(...) igraph::shortest.paths(...)
+    .moneca_env$moneca_average_path_length <- function(...) igraph::average.path.length(...)
+    .moneca_env$moneca_components <- function(...) igraph::clusters(...)
   }
   
   # Handle norm_coords which was removed in newer versions
   if (exists("norm_coords", where = asNamespace("igraph"), mode = "function")) {
-    moneca_norm_coords <<- function(...) igraph::norm_coords(...)
+    .moneca_env$moneca_norm_coords <- function(...) igraph::norm_coords(...)
   } else {
-    moneca_norm_coords <<- function(layout, xmin = -1, xmax = 1, ymin = -1, ymax = 1) {
+    .moneca_env$moneca_norm_coords <- function(layout, xmin = -1, xmax = 1, ymin = -1, ymax = 1) {
       # Normalize layout coordinates to specified range
       if (ncol(layout) != 2) {
         stop("Layout must be a two-column matrix")
@@ -61,3 +64,12 @@
 
 # Initialize compatibility functions when loaded
 .setup_igraph_compat()
+
+# Export wrapper functions that access the environment
+moneca_graph_from_adjacency <- function(...) .moneca_env$moneca_graph_from_adjacency(...)
+moneca_get_edgelist <- function(...) .moneca_env$moneca_get_edgelist(...)
+moneca_graph_density <- function(...) .moneca_env$moneca_graph_density(...)
+moneca_shortest_paths <- function(...) .moneca_env$moneca_shortest_paths(...)
+moneca_average_path_length <- function(...) .moneca_env$moneca_average_path_length(...)
+moneca_components <- function(...) .moneca_env$moneca_components(...)
+moneca_norm_coords <- function(...) .moneca_env$moneca_norm_coords(...)
